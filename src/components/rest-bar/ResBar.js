4eb91d6ddc285a501ca1f7578666d4e300885dd7
Sagni-prog <sagnialemayehu69@gmail.com>
2022-12-30 03:44:17 -0500

    resBar component modified

---
+++ src/components/rest-bar/ResBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './resBar.module.css'
+
+const ResBar = () => {
+  return (
+    <div className={styles.color}>ResBar</div>
+  )
+}
+
+export default ResBar
