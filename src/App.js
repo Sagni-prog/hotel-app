c593b6503b72714df6fdbfc17c806e4b10aa8fff
Sagni-prog <sagnialemayehu69@gmail.com>
2022-12-30 06:42:39 -0500

    contact and testimonial components added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import RestBar from './components/restBar/RestBar';
 import Hero from './components/hero/Hero';
 import Nav from './components/nav/Nav';
+import Service from './components/services/Service';
 import Room from './components/rooms/Room';
 import Footer from './components/footer/Footer';
+import Testimonial from './components/testimonial/Testimonial';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
         <Nav />
         <Hero />
+        <Service />
         <RestBar />
         <Room />
+        <Testimonial />
+        <Contact />
         <Footer />
     </div>
   );
