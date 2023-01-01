b35b4858880bf74018cd76fb5ddc5ef6d038947d
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 22:35:48 +0300

    for Net

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './hero.css'
+import Images from '../../constants/Images'
+
+const { restBar } = Images
 
 const Hero = () => {
   return (
-    <div className='hero__font hero__color'>Hero</div>
+    <div className='hero__font hero__color'>
+        <img src = { restBar } alt = "" />
+    </div>
   )
 }
 
