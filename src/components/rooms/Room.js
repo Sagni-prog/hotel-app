724c97a44dac59595a2f7dd14bbc7c50e5e72afe
Natnael Getachew <natnaeln4dgmail.com>
2023-01-01 21:38:43 +0300

    adding src/components/rooms/Room.js file

--- src/components/rooms/Room.js
+++ src/components/rooms/Room.js
@@ -12,6 +12,17 @@ const Room = () => {
           </div>
       </div>
     </div>
+    <div class="col-md-6 col-lg-4 d-flex align-items-stretch aos-init aos-animate" data-aos="flip-left" data-aos-delay="100" data-aos-duration="1000">
+<div class="room-wrap d-md-flex flex-md-column-reverse">
+<a href="room-single.html" class="img img-room" style="background-image: url(images/room-1.jpg);">
+</a>
+<div class="text p-5 text-center">
+<h3><a href="room-single.html">Suite Room</a></h3>
+<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+<p class="mb-0 mt-2"><span class="me-3 price">$450 <small>/ night</small></span><a href="#" class="btn-custom">Book Now</a></p>
+</div>
+</div>
+</div>
    
     {/* <div className='room__color room__font'>Room</div> */}
   </div>
