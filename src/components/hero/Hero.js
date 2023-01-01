a8ebfe4ea3156cb5de0111fa104aaa117785da8e
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-01-01 22:01:30 +0300

    Started navigation

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="hero__header">
       <a href="#" className="hero__">
-        <img src="" alt="" className="hero__logo"></img>
+        <img src="" className="hero__logo"></img>
       </a>
     </div>
   );
