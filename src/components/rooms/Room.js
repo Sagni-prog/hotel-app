3ee5d5d2937aa71d6e3cbec61e4d855283b07720
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 21:06:59 +0300

    for Gi

--- src/components/rooms/Room.js
+++ src/components/rooms/Room.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './room.css'
+import images from '../../constants/Images'
+
+const { room_1 } = images
 
 const Room = () => {
   return (
-    <div className='room__color room__font'>Room</div>
+    <div className='room__color room__font'>
+      <img src= {room_1} alt = '' />
+    </div>
   )
 }
 
--- src/constants/Images.js
+++ src/constants/Images.js
@@ -2,9 +2,11 @@ import cake from '../assets/images/cake.jpg'
 import vodka from '../assets/images/vodka.jpg'
 import resBar from '../assets/images/restBar.jpg'
 import burger from '../assets/images/burger.jpg'
+import room_1 from '../assets/images/room_1.jpg'
 export default{
     cake,
     vodka,
     resBar,
-    burger
+    burger,
+    room_1
 }
