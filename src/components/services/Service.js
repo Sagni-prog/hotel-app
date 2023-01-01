d5a595b325de6244108a29dfddb6cc43c539e5a4
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 23:19:19 +0300

    common css properties added

--- src/components/services/Service.js
+++ src/components/services/Service.js
@@ -5,6 +5,11 @@ const Service = () => {
   return (
     <div className='service__bg__gray'>
         <div className='container'>
+           <div  className='service__flex service__align__center'>
+                <h1 className='font__montserrat service__color__golden'>
+                    Service we provide
+                </h1>
+           </div>
              <div className='service__grid service__grid__col__3'>
                   
              </div>
