5616f7e3598ffc8fad30d76e68289d6782116314
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 21:42:18 +0300

    for Gi

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -2,9 +2,11 @@ import cake from '../assets/images/cake.jpg'
 import blackLable from '../assets/images/vodka.jpg'
 import restBar from '../assets/images/restBar.jpg'
 import burger from '../assets/images/burger.jpg'
+import room_1 from '../assets/images/room_1.jpg'
 export default{
     cake,
     blackLable,
     restBar,
-    burger
+    burger,
+    room_1
 }
