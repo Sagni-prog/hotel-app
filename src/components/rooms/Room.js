40027980b317129029a393ad6f6e030b8735704f
Natnael Getachew <natnaeln4dgmail.com>
2023-01-01 20:51:43 +0300

    testing from ji

--- src/components/rooms/Room.js
+++ src/components/rooms/Room.js
@@ -3,7 +3,19 @@ import './room.css'
 
 const Room = () => {
   return (
-    <div className='room__color room__font'>Room</div>
+  <div className='room__container room_container_bg'>
+    <div className='container'>
+       <div className='room_header'>
+          <div className="room_heading_section">
+             <span className="subheading">Our Rooms</span>
+             <h2>Featured Rooms</h2>
+          </div>
+      </div>
+    </div>
+   
+    {/* <div className='room__color room__font'>Room</div> */}
+  </div>
+    
   )
 }
 
