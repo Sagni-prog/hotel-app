575850b8368dd840ed1c7a13fa15bf50ae568866
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-01 21:33:48 +0300

    solved

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,10 +1,10 @@
 import cake from '../assets/images/cake.jpg'
-import vodka from '../assets/images/vodka.jpg'
+import blackLable from '../assets/images/vodka.jpg'
 import resBar from '../assets/images/restBar.jpg'
 import burger from '../assets/images/burger.jpg'
 export default{
     cake,
-    vodka,
+    blackLable,
     resBar,
     burger
 }
