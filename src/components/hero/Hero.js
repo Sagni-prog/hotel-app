b267839dd8e636b2d7ee4b0398ee2c3c8557d012
natty-7 <nati7fekadu@gmail.com>
2023-01-01 23:28:42 +0300

    h

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,30 +1,21 @@
-<<<<<<< HEAD
 import React from "react";
 import "./hero.css";
+import Images from "../../constants/images";
+
+const { restBar } = Images;
 
 const Hero = () => {
   return (
     <div className="hero__header">
-      <a href="#" className="hero__">
-        <img src="" className="hero__logo"></img>
+      <a
+        href="#"
+        className="hero__">
+        <img
+          src=""
+          className="hero__logo"></img>
       </a>
     </div>
   );
 };
-=======
-import React from 'react'
-import './hero.css'
-import Images from '../../constants/Images'
-
-const { restBar } = Images
-
-const Hero = () => {
-  return (
-    <div className='hero__font hero__color'>
-        <img src = { restBar } alt = "hello" />
-    </div>
-  )
-}
->>>>>>> 6e14543867eebf3eb483e9a9ede3f2db4b47066a
 
 export default Hero;
