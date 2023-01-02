41afd43aeefb37d58a2648cb5630d8bbfa603647
Natnael Getachew <natnaeln4dgmail.com>
2023-01-02 09:16:57 +0300

    updating all file

--- src/components/rooms/Room.js
+++ src/components/rooms/Room.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './room.css'
+import images from '../../constants/images'
 
 const Room = () => {
+  const { room_1 } = images;
   return (
   <div className='room__container room_container_bg'>
     <div className='container'>
@@ -13,24 +15,43 @@ const Room = () => {
       </div>
     </div>
     <div className='rooms_card_container'>
-    <div class="room-wrap d-md-flex flex-md-column-reverse">
-<a href="room-single.html" class="img img-room">
-</a>
-<div class="text p-5 text-center">
-<h3><a href="#">Suite Room</a></h3>
-<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-<p class="mb-0 mt-2"><span class="me-3 price">$450 <small>/ night</small></span><a href="#" class="btn-custom">Book Now</a></p>
-</div>
-</div>
-<div class="room-wrap">
-<div class="text p-5 text-center">
-<h3><a href="room-single.html">Suite Room</a></h3>
-<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-<p class="mb-0 mt-2"><span class="me-3 price">$450 <small>/ night</small></span><a href="#" class="btn-custom">Book Now</a></p>
-</div>
-<a href="#" class="img img-room">
-</a>
-</div>
+          <div className="room-wrap">
+           <a href="#" className="img img-room">
+           <img
+        src={room_1}
+        alt=""
+              />
+              </a>
+            <div className="text p-5 text-center">
+              <h3><a href="#">Suite Room</a></h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <p className="mb-0 mt-2"><span className="me-3 price">$450 <small>/ night</small></span><a href="#" className="btn-custom">Book Now</a></p>
+             </div>
+          </div>
+        <div className="room-wrap">
+            <div className="text p-5 text-center">
+              <h3><a href="#">Suite Room</a></h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <p className="mb-0 mt-2"><span className="me-3 price">$450 <small>/ night</small></span><a href="#" className="btn-custom">Book Now</a></p>
+             </div>
+             <a href="#" className="img img-room">
+              </a>
+          </div>
+          <div className="room-wrap">
+           <a href="#" className="img img-room">
+                <img
+        src={room_1}
+        alt=""
+      />
+              </a>
+            <div className="text p-5 text-center">
+              <h3><a href="#">Suite Room</a></h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <p className="mb-0 mt-2"><span className="me-3 price">$450 <small>/ night</small></span><a href="#" className="btn-custom">Book Now</a></p>
+             </div>
+          </div>
+       
+
     </div>
    
     {/* <div className='room__color room__font'>Room</div> */}
@@ -39,4 +60,5 @@ const Room = () => {
   )
 }
 
-export default Room
+
+export default Room;
